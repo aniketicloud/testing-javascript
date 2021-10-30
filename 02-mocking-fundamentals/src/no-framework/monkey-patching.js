682d16baad7c03c476136930cff210d69265dedb
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-31 00:00:47 +0530

    cleaning up so other modules will get unmodified state

--- 02-mocking-fundamentals/src/no-framework/monkey-patching.js
+++ 02-mocking-fundamentals/src/no-framework/monkey-patching.js
@@ -2,6 +2,8 @@ const assert = require("assert");
 const thumbWar = require("../thumb-war");
 const utils = require("../utils");
 
+const originalGetWinner = utils.getWinner;
+
 // mocking to get first argument as winner always
 utils.getWinner = (p1, p2) => p1;
 
@@ -9,3 +11,4 @@ const winner = thumbWar("Nike", "Percy");
 assert.strictEqual(winner, "Nike");
 
 // cleanup
+utils.getWinner = originalGetWinner;
