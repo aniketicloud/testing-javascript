157aa3f14caac5e95f5741199f834b947d7656ff
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-30 23:58:47 +0530

    mocking to get first argument as winner always

--- 02-mocking-fundamentals/src/no-framework/monkey-patching.js
+++ 02-mocking-fundamentals/src/no-framework/monkey-patching.js
@@ -2,6 +2,9 @@ const assert = require("assert");
 const thumbWar = require("../thumb-war");
 const utils = require("../utils");
 
+// mocking to get first argument as winner always
+utils.getWinner = (p1, p2) => p1;
+
 const winner = thumbWar("Nike", "Percy");
 assert.strictEqual(winner, "Nike");
 
