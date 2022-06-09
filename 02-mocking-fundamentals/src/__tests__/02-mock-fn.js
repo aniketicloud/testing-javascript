dbf43be30c5e9eea79de41cb82db940636d4f384
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-06-09 09:57:52 +0530

    m02-mock-fn: checking length of args

--- 02-mocking-fundamentals/src/__tests__/02-mock-fn.js
+++ 02-mocking-fundamentals/src/__tests__/02-mock-fn.js
@@ -3,10 +3,12 @@ const utils = require("../utils");
 
 test("returns winner", () => {
   const originalGetWinner = utils.getWinner;
-  utils.getWinner = (p1, p2) => p1;
+  utils.getWinner = jest.fn((p1, p2) => p1);
 
   const winner = thumbWar("Nike", "Percy");
   expect(winner).toBe("Nike");
+  expect(utils.getWinner).toHaveBeenCalledTimes(2);
+  expect(utils.getWinner).toHaveBeenCalledWith("Nike", "Percy");
 
-  utils.getWinner = originalGetWinner;
+  // utils.getWinner = originalGetWinner;
 });
