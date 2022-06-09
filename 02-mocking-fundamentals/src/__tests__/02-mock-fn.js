4fd8d193e0a335c0fa370d3473f7247a0b552754
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-06-09 10:06:05 +0530

    m02-mock-fn: checking number of calls, args

--- 02-mocking-fundamentals/src/__tests__/02-mock-fn.js
+++ 02-mocking-fundamentals/src/__tests__/02-mock-fn.js
@@ -4,11 +4,15 @@ const utils = require("../utils");
 test("returns winner", () => {
   const originalGetWinner = utils.getWinner;
   utils.getWinner = jest.fn((p1, p2) => p1);
+  const nike = "Nike";
+  const percy = "Percy";
 
-  const winner = thumbWar("Nike", "Percy");
-  expect(winner).toBe("Nike");
+  const winner = thumbWar(nike, percy);
+  expect(winner).toBe(nike);
+  console.log(utils.getWinner.mock);
   expect(utils.getWinner).toHaveBeenCalledTimes(2);
-  expect(utils.getWinner).toHaveBeenCalledWith("Nike", "Percy");
-
-  // utils.getWinner = originalGetWinner;
+  expect(utils.getWinner).toHaveBeenCalledWith(nike, percy);
+  expect(utils.getWinner).toHaveBeenNthCalledWith(1, nike, percy);
+  expect(utils.getWinner).toHaveBeenNthCalledWith(2, nike, percy);
+  utils.getWinner = originalGetWinner;
 });
